edbc451c55c2150a304d0f43bcc59afc4b9e49e8
Lee Palacios <vitualizz@gmail.com>
2020-04-19 15:46:59 -0500

    First page

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -1,9 +1,28 @@
 <template lang='pug'>
   .has-text-centered
-    h5 Eres
+    img(":src"='logoImg').logo-img
+    p.text Developer / Design
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      logoImg: require('@/assets/imgs/Resources/logo_img.png')
+    }
+  }
+}
+</script>
+
 <style scoped lang='sass'>
-h5
+.logo-img
+  border-radius: 100%
+  width: 30vh
+.text
+  margin-top: 20px
+  letter-spacing: 5px
   color: $color-blank
+  font-family: $font-manjari
+  font-weight: $weight-regular
+  font-size: 2rem
 </style>
