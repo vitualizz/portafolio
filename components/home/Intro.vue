31791fd623ad8e2f5e7dd41ad6f65abd2ea01c42
Lee Palacios <vitualizz@gmail.com>
2020-04-19 16:40:15 -0500

    typeit

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -1,15 +1,28 @@
 <template lang='pug'>
   .has-text-centered
     img(":src"='logoImg').logo-img
-    p.text Developer / Design
+    p.text
 </template>
 
 <script>
+import TypeIt from 'typeit'
+
 export default {
   data () {
     return {
       logoImg: require('@/assets/imgs/Resources/logo_img.png')
     }
+  },
+  mounted () {
+    new TypeIt('.text', {
+      strings: [
+        '@lee.develop',
+        'Desarrollador Web',
+        'Yo ❤️  Tecnologia'
+      ],
+      loop: true,
+      breakLines: false
+    }).go()
   }
 }
 </script>
@@ -20,9 +33,9 @@ export default {
   width: 30vh
 .text
   margin-top: 20px
-  letter-spacing: 5px
+  letter-spacing: 3px
   color: $color-blank
   font-family: $font-manjari
-  font-weight: $weight-regular
+  font-weight: $weight-bold
   font-size: 2rem
 </style>
