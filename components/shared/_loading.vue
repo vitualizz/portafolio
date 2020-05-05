b9cdfdfdc12c49ff91d4f350050db0c16361f25b
Lee Palacios <vitualizz@gmail.com>
2020-05-05 11:08:34 -0500

    loader

---
+++ components/shared/_loading.vue
@@ -0,0 +1,55 @@
+<template lang='pug'>
+  .loading-wrapper(v-show='show')
+    .loader-main
+      img.image-load(:src='logoImg')
+</template>
+
+<script>
+export default {
+  props: {
+    logoImg: String
+  },
+  data () {
+    return {
+      show: true
+    }
+  },
+  mounted () {
+    this.loadingComplete()
+  },
+  methods: {
+    loadingComplete () {
+      const self = this
+      $(window).on('load', function () {
+        setTimeout(function () {
+          self.show = false
+        }, 2000)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+.loading-wrapper
+  position: absolute
+  z-index: 999
+  width: 100%
+  height: 100%
+  display: flex
+  background: #fafafa
+  .loader-main
+    margin: auto
+    .image-load
+      width: 250px
+      animation: 3s infinite ease-in-out floating
+
+@keyframes floating
+  from
+    transform: translate(0, 0px)
+  65%
+    transform: translate(0, 20px)
+  to
+    transform: translate(0, 0px)
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <Loading
+      :logoImg='logoImg'
+    />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Loading from '@/components/shared/_loading.vue'
+export default {
+  components: {
+    Loading
+  },
+  data () {
+    return {
+      logoImg: require('@/assets/imgs/Resources/logo_letter.png')
+    }
+  }
+}
+</script>
